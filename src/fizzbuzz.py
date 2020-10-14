9108f6726756c84912ad79a1cba5e55d23bc62cb
Dan Putman <dputman@users.noreply.github.com>
2020-10-14 15:26:11 -0400

    Update fizzbuzz.py

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -5,6 +5,6 @@ class FizzBuzz:
         if input % 3 == 0:
             output += "Fizz"
         if input % 5 == 0:
-            output += "Buzz"
+            output += "Buzz!"
 
         return output if len(output) else str(input) 
